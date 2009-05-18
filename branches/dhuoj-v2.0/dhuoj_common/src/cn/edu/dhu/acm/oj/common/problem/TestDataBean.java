03409010ba2791837d3eb763dd91d1d689970c53
dazhuxiaokai <dazhuxiaokai@636ca296-8340-0410-837b-7fac97c75235>
2009-05-18 07:45:47 +0000

    Modified by Zhu Kai.
    Adding two methods to TestDataBean:
        getSingleCaseInput() and getSingleCaseOutput().
    
    These two methods can provide all test data, each as a complete single-case test data.

--- branches/dhuoj-v2.0/dhuoj_common/src/cn/edu/dhu/acm/oj/common/problem/TestDataBean.java
+++ branches/dhuoj-v2.0/dhuoj_common/src/cn/edu/dhu/acm/oj/common/problem/TestDataBean.java
@@ -182,6 +182,108 @@ public final class TestDataBean extends NodeBean {
         root.setChildText("TimeLimit", str);
     }
 
+
+    /**
+     * Get the input data of all test cases,
+     * each as a complete single-case input data.
+     *
+     * @return Array of input data of all the test cases.
+     *
+     * @author Zhu Kai
+     */
+    public String[] getSingleCaseInputs() {
+        InputFormatBean inputFormat = getInputFormat();
+        SeperatorBean seperator = inputFormat.getSeperator();
+        String inner = seperator.getText();
+        String layout = seperator.getLayout();
+
+        String terminator = inputFormat.getTerminator();
+
+        String inputType = inputFormat.getInputType();
+
+        List testCaseList = getTestCaseList();
+        String[] result = new String[ testCaseList.size() ];
+
+        String lineBreak = System.getProperty("line.separator");
+
+        int i = 0;
+        Iterator it = testCaseList.iterator();
+        while ( it.hasNext() ) {
+            TestCaseBean testCase = new TestCaseBean( (Element)( it.next() ) );
+            testCase.setArchiveDirectory(this.archiveDirectory);
+
+            StringBuffer strBuf = new StringBuffer();
+
+            if ( inputType.equals("num") )
+                strBuf.append("1" + lineBreak);
+            
+            if ( layout.equals("top") || layout.equals("both") )
+                strBuf.append(inner);
+
+            strBuf.append( testCase.getTestOutput() );
+
+            if ( layout.equals("bottom") || layout.equals("both") )
+                strBuf.append(inner);
+
+            if (inputType.equals("zero"))
+                strBuf.append(terminator);
+
+            result[i] = strBuf.toString();
+
+            i++;
+        }
+
+        return result;
+    }
+
+    /**
+     * Get the output data of all test cases,
+     * each as a complete single-case output data.
+     *
+     * @return Array of output data of all the test cases.
+     *
+     * @author Zhu Kai
+     */
+    public String[] getSingleCaseOutputs() {
+        OutputFormatBean outputFormat = getOutputFormat();
+        String header = outputFormat.getHeader();
+        String footer = outputFormat.getFooter();
+
+        SeperatorBean seperator = outputFormat.getSeperator();
+        String inner = seperator.getText();
+        String layout = seperator.getLayout();
+
+        List testCaseList = getTestCaseList();
+        String[] result = new String[ testCaseList.size() ];
+        
+        int i = 0;
+        Iterator it = testCaseList.iterator();
+        while ( it.hasNext() ) {
+            TestCaseBean testCase = new TestCaseBean( (Element)( it.next() ) );
+            testCase.setArchiveDirectory(this.archiveDirectory);
+
+            StringBuffer strBuf = new StringBuffer();
+
+            strBuf.append(header);
+
+            if ( layout.equals("top") || layout.equals("both") )
+                strBuf.append(inner);
+
+            strBuf.append( testCase.getTestOutput() );
+
+            if ( layout.equals("bottom") || layout.equals("both") )
+                strBuf.append(inner);
+
+            strBuf.append(footer);
+
+            result[i] = strBuf.toString();
+
+            i++;
+        }
+
+        return result;
+    }
+
     /**
      * Get the test case at index i.
      *
