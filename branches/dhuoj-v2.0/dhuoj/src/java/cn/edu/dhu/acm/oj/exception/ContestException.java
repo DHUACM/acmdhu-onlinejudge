1e473048c589bd6e57e112d8a400560d93d135a2
frankhuhu.dhu <frankhuhu.dhu@636ca296-8340-0410-837b-7fac97c75235>
2009-05-28 01:11:54 +0000

    Update ContestService: Add getContestDetail interface.

---
+++ branches/dhuoj-v2.0/dhuoj/src/java/cn/edu/dhu/acm/oj/exception/ContestException.java
@@ -0,0 +1,12 @@
+package cn.edu.dhu.acm.oj.exception;
+
+public class ContestException extends Exception {
+
+    public ContestException() {
+    }
+
+    public ContestException(String msg) {
+        super(msg);
+    }
+
+}
