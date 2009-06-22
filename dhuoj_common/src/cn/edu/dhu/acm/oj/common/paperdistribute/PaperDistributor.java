24ecb0cfcbfaf012eb9412b0030ccde41c6b2515
dazhuxiaokai <dazhuxiaokai@636ca296-8340-0410-837b-7fac97c75235>
2009-06-22 15:25:37 +0000

    1. Adding a private method updateProblemList(), going to replace some common codes in several methods. But no harm now.
    2. Adding a paper distributor package.

---
+++ dhuoj_common/src/cn/edu/dhu/acm/oj/common/paperdistribute/PaperDistributor.java
@@ -0,0 +1,18 @@
+package cn.edu.dhu.acm.oj.common.paperdistribute;
+
+import java.io.IOException;
+
+import cn.edu.dhu.acm.oj.common.paper.PaperBean;
+
+/**
+ * The class provides the function of distributing a paper. The distributed
+ * paper is encrypted with a public key and a private key. The public key
+ * must be provided. If the private key is not provided, a random private
+ * key is generated.
+ * 
+ * @author Zhu Kai
+ * 
+ * @since SVN 93
+ */
+public class PaperDistributor {
+}
