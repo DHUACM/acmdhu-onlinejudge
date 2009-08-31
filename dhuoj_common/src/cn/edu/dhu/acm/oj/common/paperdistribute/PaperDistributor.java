0f8e7e686da2db49fe0a3adc9831e29d15fe5a64
dazhuxiaokai <dazhuxiaokai@636ca296-8340-0410-837b-7fac97c75235>
2009-08-31 03:56:24 +0000

    Changing the paper distributor and decryptor. Now can encrypt and decrypt single file with single password.

--- dhuoj_common/src/cn/edu/dhu/acm/oj/common/paperdistribute/PaperDistributor.java
+++ dhuoj_common/src/cn/edu/dhu/acm/oj/common/paperdistribute/PaperDistributor.java
@@ -17,12 +17,65 @@ import cn.edu.dhu.acm.oj.common.util.Cryptograph;
  * 
  * @author Zhu Kai
  * 
- * @version SVN 96
+ * @version SVN 107
  * 
  * @since SVN 93
  */
 public class PaperDistributor {
-
+    
+    /**
+     * The main method to encrypt a file.
+     */
+    public static void main( String[] args ) {
+        if (3 != args.length) {
+            System.err.println(
+                "Usage java cn.edu.dhu.acm.oj.common.paperdistribute" +
+                ".PaperDistributor filename output-directory password");
+            
+            System.exit(1);
+        }
+        try {
+            File paperFile = new File( args[0] );
+            File outputDirectory = new File( args[1] );
+            String password = args[2];
+            
+            distributePaper(paperFile, outputDirectory, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
+    
+    
+    /**
+     * Encrypting a paper file with a password. The paper file is encrypted
+     * with AES algorithm, and output to the specific directory. The output
+     * filename is like the original paper filename, only the extension
+     * name changed to {@code pae}.
+     * 
+     * @param paperFile XML file of the paper to be distributed.
+     * @param outputDirectory the directory where the encrypted paper file
+     *            are put.
+     * @param password the password to encrypt/decrypt the paper.
+     * 
+     * @throws IOException if any error occurs during file writing.
+     */
+    public static void distributePaper(
+        File paperFile, File outputDirectory, String password)
+    throws IOException {
+        // Changing the filename extension.
+        String filename = paperFile.getName();
+        int dotIndex = filename.lastIndexOf('.');
+        if ( -1 != dotIndex ) {
+            filename = filename.substring(0, dotIndex + 1) + "pae";
+        }
+        File outFile = new File(outputDirectory, filename);
+        
+        encryptFile(paperFile, outFile, password);
+    }
+    
     /**
      * Distributing a paper's XML file to each user. Each user will have a
      * paper file encrypted with a key combined by his own password and a
@@ -57,36 +110,30 @@ public class PaperDistributor {
 
 
     /**
-     * Encrypting a paper for a specific user. The paper file is encrypted
-     * with AES algorithm, and output to the specific directory. The output
-     * filename is <i>userName.pae</i> while <i>userName</i> is the name of
-     * the user, specified by the {@code userName} parameter. The
-     * encryption key is the MD5 digest generated from the concatenation of
-     * the paper's password and the user's password.
+     * Encrypting a file with a password, using AES algorithm. The result
+     * file is output to the specific directory, and the output filename is
+     * like the original paper filename, only the extension name changed to
+     * {@code pae}.The encryption key is the MD5 digest generated from the
+     * password.
      * 
-     * @param paperFile the paper file to encrypt.
-     * @param outputDirectory the directory to put the encrypted file.
-     * @param paperPassword the password of the paper.
-     * @param userName the user's name.
-     * @param userPassword the user's password.
+     * @param inFile the file to encrypt.
+     * @param outFile the output file.
+     * @param password the password used in encryption.
      * 
-     * @throws IOException if any error occur during file I/O.
+     * @throws IOException if any error occurs during file I/O.
      */
-    private static void encryptPaperFile(
-        File paperFile, File outputDirectory,
-        String paperPassword, String userName, String userPassword)
+    private static void encryptFile(
+        File inFile, File outFile, String password)
     throws IOException {
-        String concat = paperPassword.concat(userPassword);
-        byte[] key = Cryptograph.stringMD5(concat);
+        byte[] key = Cryptograph.stringMD5(password);
         
-        if ( !outputDirectory.isDirectory() ) {
-            outputDirectory.mkdirs();
+        File dir = outFile.getParentFile();
+        if ( !dir.isDirectory() ) {
+            dir.mkdirs();
         }
         
-        File outFile = new File(outputDirectory, userName + ".pae");
-        
         BufferedInputStream inputStream = new BufferedInputStream(
-            new FileInputStream(paperFile) );
+            new FileInputStream(inFile) );
         BufferedOutputStream outputStream = new BufferedOutputStream(
             new FileOutputStream(outFile) );
         
@@ -100,6 +147,32 @@ public class PaperDistributor {
         inputStream.close();
         //the output stream is closed by the aesEncrypt method, so needn't
         //close it here.
+    }
+    
+    /**
+     * Encrypting a paper for a specific user. The paper file is encrypted
+     * with AES algorithm, and output to the specific directory. The output
+     * filename is <i>userName.pae</i> while <i>userName</i> is the name of
+     * the user, specified by the {@code userName} parameter. The
+     * encryption key is the MD5 digest generated from the concatenation of
+     * the paper's password and the user's password.
+     * 
+     * @param paperFile the paper file to encrypt.
+     * @param outputDirectory the directory to put the encrypted file.
+     * @param paperPassword the password of the paper.
+     * @param userName the user's name.
+     * @param userPassword the user's password.
+     * 
+     * @throws IOException if any error occurs during file I/O.
+     */
+    private static void encryptPaperFile(
+        File paperFile, File outputDirectory,
+        String paperPassword, String userName, String userPassword)
+    throws IOException {
+        String filename = userName + ".pae";
+        File outFile = new File(outputDirectory, filename);
+        String concat = paperPassword.concat(userPassword);
+        encryptFile(paperFile, outFile, concat);
     }
 
 }
