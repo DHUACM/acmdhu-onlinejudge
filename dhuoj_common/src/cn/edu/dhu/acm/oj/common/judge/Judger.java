2fca0d837f73a7adae9fd4654bf891c0d69dd869
sun8421 <sun8421@636ca296-8340-0410-837b-7fac97c75235>
2009-08-31 09:31:33 +0000

    Change judge's malicious, and some JAVA malicious. change the bug of null test case.

--- dhuoj_common/src/cn/edu/dhu/acm/oj/common/judge/Judger.java
+++ dhuoj_common/src/cn/edu/dhu/acm/oj/common/judge/Judger.java
@@ -5,48 +5,48 @@ import cn.edu.dhu.acm.oj.common.config.*;
 
 public class Judger {
 
-    public Judger(RunBean rb, EnvironmentBean eb) {
-        rbean = rb;
-        envbean = eb;
-    }
+	public Judger(RunBean rb, EnvironmentBean eb) {
+		rbean = rb;
+		envbean = eb;
+	}
 
-    public String getCompileinfo() {
-        return compileinfo;
-    }
+	public String getCompileinfo() {
+		return compileinfo;
+	}
 
-    public boolean Compile() {
-        Compile cc = new Compile(rbean, envbean);
-        compile = cc.doit();
-        if (compile) {
-            CheckMaliciousCode cmc = new CheckMaliciousCode(rbean);
-            compile = cmc.checkKeyWord();
-        }
-        compileinfo = rbean.getCompileInfo();
-        return compile;
-    }
+	public boolean Compile() {
+		CheckMaliciousCode cmc = new CheckMaliciousCode(rbean);
+		compile = cmc.checkKeyWord();
+		if (compile) {
+			Compile cc = new Compile(rbean, envbean);
+			compile = cc.doit();
+		}
+		compileinfo = rbean.getCompileInfo();
+		return compile;
+	}
 
-    public void Run() {
-        Run rr = new Run(rbean, envbean);
-        rr.start();
-        try {
-            rr.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public void Run() {
+		Run rr = new Run(rbean, envbean);
+		rr.start();
+		try {
+			rr.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void Check() {
-        if (rbean.getResult() == Const.QUEUE) {
-            CheckAnswer ca = new CheckAnswer();
-            ca.setAnswer(rbean.getOutput());
-            ca.setStandarAnswer(rbean.getStdAns());
-            ca.AnswerCheck();
-            rbean.setResult(ca.getVerdict());
-            rbean.setPercent(ca.getPercent());
-        }
-    }
-    private RunBean rbean;
-    private EnvironmentBean envbean;
-    private boolean compile;
-    private String compileinfo;
+	public void Check() {
+		if (rbean.getResult() == Const.QUEUE) {
+			CheckAnswer ca = new CheckAnswer();
+			ca.setAnswer(rbean.getOutput());
+			ca.setStandarAnswer(rbean.getStdAns());
+			ca.AnswerCheck();
+			rbean.setResult(ca.getVerdict());
+			rbean.setPercent(ca.getPercent());
+		}
+	}
+	private RunBean rbean;
+	private EnvironmentBean envbean;
+	private boolean compile;
+	private String compileinfo;
 }
