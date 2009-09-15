307afd07b101f23bf212759dba365bcbeb9b8d3b
sun8421 <sun8421@636ca296-8340-0410-837b-7fac97c75235>
2009-09-15 12:49:49 +0000

    Change Const and Run.java and contest0.xml

--- branches/dhuoj-v2.0/dhuoj_common/src/cn/edu/dhu/acm/oj/common/judge/Run.java
+++ branches/dhuoj-v2.0/dhuoj_common/src/cn/edu/dhu/acm/oj/common/judge/Run.java
@@ -6,96 +6,102 @@ import java.io.*;
 
 public class Run extends Thread {
 
-    public Run(RunBean rb, EnvironmentBean eb) {
-        runbean = rb;
-        String lan = Const.LANGUAGE[rb.getLanguage()];
-        commandline = eb.getRunCmd(lan, Const.COMPILENAME);
-    }
+	public Run(RunBean rb, EnvironmentBean eb) {
+		runbean = rb;
+		String lan = Const.LANGUAGE[rb.getLanguage()];
+		commandline = eb.getRunCmd(lan, Const.COMPILENAME);
+	}
 
-    private void InputData(OutputStream os)
-            throws IOException {
-        os.write(runbean.getInput().getBytes());
-        os.close();
-    }
+	private void InputData(OutputStream os)
+			throws IOException {
+		os.write(runbean.getInput().getBytes());
+		os.close();
+	}
 
-    @Override
-    public void destroy() {
-        try {
-            runbean.setResult(Const.TLE);
-            tend = System.currentTimeMillis();
-            runbean.setTimeUsed(tend - tbegin);
-            pro.destroy();
-            pin.close();
-            pout.close();
-            perr.close();
-        } catch (IOException IOE) {
-            System.out.println(IOE.toString());
-        }
-    }
+	@Override
+	public void destroy() {
+		try {
+			runbean.setResult(Const.TLE);
+			tend = System.currentTimeMillis();
+			runbean.setTimeUsed(tend - tbegin);
+			pro.destroy();
+			pin.close();
+			pout.close();
+			perr.close();
+		} catch (IOException IOE) {
+			System.out.println(IOE.toString());
+		}
+	}
 
-    @Override
-    public void run() {
-        try {
-            long tle = runbean.getTimeLimit();
-            if (runbean.getLanguage() == Const.JAVA) {
-                tle = tle * Const.JAVA_LIMIT;
-            }
-            tk = new TimeoutKill(tle);
-            System.out.println("Run : " + commandline);
-            pro = Runtime.getRuntime().exec(commandline);
-            tbegin = System.currentTimeMillis();
-            pin = pro.getOutputStream();
-            pout = pro.getInputStream();
-            perr = pro.getErrorStream();
-            errris = new ReadInputStream(perr);
-            outris = new ReadInputStream(pout);
+	@Override
+	public void run() {
+		long tle = 1;
+		try {
+			tle = runbean.getTimeLimit();
+			if (runbean.getLanguage() == Const.JAVA) {
+				tle = tle * Const.JAVA_LIMIT;
+			}
+			tk = new TimeoutKill(tle);
+			System.out.println("Run : " + commandline);
+			pro = Runtime.getRuntime().exec(commandline);
+			tbegin = System.currentTimeMillis();
+			pin = pro.getOutputStream();
+			pout = pro.getInputStream();
+			perr = pro.getErrorStream();
+			errris = new ReadInputStream(perr);
+			outris = new ReadInputStream(pout);
 
-            tk.setTestThread(this);
-            errris.start();
-            outris.start();
-            tk.start();
+			tk.setTestThread(this);
+			errris.start();
+			outris.start();
+			tk.start();
 
-            InputData(pin);
-            pro.waitFor();
-            outris.join();
-            errris.join();
+			InputData(pin);
+			pro.waitFor();
+			outris.join();
+			errris.join();
 
-            tend = System.currentTimeMillis();
-            runbean.setTimeUsed(tend - tbegin);
-            int exitValue = pro.exitValue();
-            if (exitValue == 0) {
-                tk.stop();
-                runbean.setOutput(outris.getMessage());
-                if (outris.getMessage().length() >= Const.FILEMAXSIZE) {
-                    runbean.setResult(Const.OLE);
-                }
-            } else {
-                tk.stop();
-                runbean.setResult(Const.RE);
-                runbean.setOutput(outris.getMessage());
-            }
-            pin.close();
-            pro.destroy();
-            System.out.println("Run Done!");
-        } catch (Exception E) {
-            tk.stop();
-            runbean.setResult(Const.RE);
-            System.out.println(E.toString());
-        }
-    }
+			tend = System.currentTimeMillis();
+			runbean.setTimeUsed(tend - tbegin);
+			int exitValue = pro.exitValue();
+			if (exitValue == 0) {
+				tk.stop();
+				runbean.setOutput(outris.getMessage());
+				if (outris.getMessage().length() >= Const.FILEMAXSIZE) {
+					runbean.setResult(Const.OLE);
+				}
+			} else {
+				tk.stop();
+				runbean.setResult(Const.RE);
+				runbean.setOutput(outris.getMessage());
+			}
+			pin.close();
+			pro.destroy();
+			System.out.println("Run Done!");
+		} catch (Exception E) {
+			tk.stop();
+			tend = System.currentTimeMillis();
+			if ((tend - tbegin) >= (tle * (long) 1000)) {
+				runbean.setResult(Const.TLE);
+			} else {
+				runbean.setResult(Const.RE);
+			}
+			System.out.println("1"+E.toString());
+		}
+	}
 
-    public RunBean getRunBean() {
-        return runbean;
-    }
-    private RunBean runbean;
-    private Process pro;
-    private OutputStream pin;
-    private InputStream perr;
-    private InputStream pout;
-    private ReadInputStream errris;
-    private ReadInputStream outris;
-    private String commandline;
-    private TimeoutKill tk;
-    private long tbegin;
-    private long tend;
+	public RunBean getRunBean() {
+		return runbean;
+	}
+	private RunBean runbean;
+	private Process pro;
+	private OutputStream pin;
+	private InputStream perr;
+	private InputStream pout;
+	private ReadInputStream errris;
+	private ReadInputStream outris;
+	private String commandline;
+	private TimeoutKill tk;
+	private long tbegin;
+	private long tend;
 }
